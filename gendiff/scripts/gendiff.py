657759371f5739f56aa718bf4def4e9a1a85e407
cyril <sokolovkirill9875@yandex.ru>
2023-10-07 11:17:33 +0300

    create makefile and add new optional argument

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
 import argparse
 
+
 def add_arg():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument(
+        '-f', '--format',
+        type=str,
+        default='FORMAT',
+        help='set format of output'
+    )
     args = parser.parse_args()
+
 
 def main():
     print(add_arg())
+
 
 if __name__ == '__main__':
     main()
